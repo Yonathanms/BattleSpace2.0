a1dd8e6fe157c6369c6c0f8474a1391cb67b3e2d
Yonathan Monge Sanabria <yonathan0146@estudiantec.cr>
2023-04-13 21:24:46 -0600

    eventos en el menu
    Se crearon eventos para los botones

--- Vista_Controlador/Ventana_Principal.h
+++ Vista_Controlador/Ventana_Principal.h
@@ -1,23 +1,34 @@
 //
 // Created by yonathanms on 08/04/23.
 //
+#pragma once
 
 #include "SFML/Graphics.hpp"
+#include "iostream"
+#include "fstream"
+
 using namespace sf;
 
 #ifndef PRUEBASPROYECTO_VENTANA_PRINCIPAL_H
 #define PRUEBASPROYECTO_VENTANA_PRINCIPAL_H
 
-#endif //PRUEBASPROYECTO_VENTANA_PRINCIPAL_H
 
 class ventana_Principal{       ///clase ventana del menu
 
 public:
-    ventana_Principal(int dimension_x, int dimension_y);      /// ahi tiene los parametros de las diemsiones que uno quiera
+    ventana_Principal();      /// ahi tiene los parametros de las diemsiones que uno quiera
     void renderizar();         /// este es el metodo renderizar
     void ciclo();               /// metodo de los ciclos
+    void botones();
+    void eventos();
 
 private:
-    RenderWindow * VentanaMenu; ////representa la ventana principal, pero a pesar de eso es un atributo, no es un objeto como tal
+    int num_dificultad; /// representa la dificultad que va a escoger el jugador seg[un el boton que aprete
+    RectangleShape * btnFacil;
+    RectangleShape  btnMedio;
+    RectangleShape  btnDificil;
+    RenderWindow  VentanaMenu; ////representa la ventana principal, pero a pesar de eso es un atributo, no es un objeto como tal
     int fps;    //// representa un id para la cantidad de frames por segundo, igualmente es un atributo y se le da valor en el .cpp
 };
+
+#endif PRUEBASPROYECTO_VENTANA_PRINCIPAL_H
