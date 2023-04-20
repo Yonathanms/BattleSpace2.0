54a1a56494db43e6334cd07b5293f4db78895537
Yonathan Monge Sanabria <yonathan0146@estudiantec.cr>
2023-04-20 06:18:06 -0600

    PNG enemigos
    Se agregaron las imagenes que corresponden a los enemigos.

--- Vista_Controlador/Enemigos.h
+++ Vista_Controlador/Enemigos.h
@@ -3,14 +3,25 @@
 //
 
 #ifndef PRUEBASPROYECTO_ENEMIGOS_H
+
+#include "SFML/Graphics.hpp"
+
 #define PRUEBASPROYECTO_ENEMIGOS_H
+
+using namespace std;
+using namespace sf;
 
 
 class Enemigos {
 
 public:
+    Enemigos();
+
 
 private:
+
+    Sprite *sprt_enemy;
+    Texture *text_enemy;
 
 };
 
